3b5d371a71ed2548881302dc9e0be23be0aa153f
katsuya yamaguchi <y.katsuya1116@gmail.com>
2024-07-02 09:21:18 +0900

    feat: meta情報追加

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,16 @@ import React from 'react';
 import './App.css';
 import SeirekiWarekiConverter from './components/SeirekiWarekiConverter';
 import AgeCalculator from './components/AgeCalculator';
+import { Helmet } from 'react-helmet';
 
 function App() {
   return (
     <div className="App">
+      <Helmet>
+        <title>KOYOMI</title>
+        <meta name="description" content="暦に関する様々なことができるサービス" />
+        <meta name="keywords" content="年齢計算, 西暦, 和暦, 変換, 干支, 今日は何の日" />
+      </Helmet>
       <SeirekiWarekiConverter />
       <AgeCalculator />
     </div>

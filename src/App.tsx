73cd637f937ca72a90149c79e10ac1fb800ed1b4
katsuya yamaguchi <y.katsuya1116@gmail.com>
2024-07-20 20:46:13 +0900

    feat: サーチコンソール用metaタグ追加

--- src/App.tsx
+++ src/App.tsx
@@ -19,6 +19,7 @@ const App: React.FC = () => {
           name="keywords"
           content="年齢計算, 西暦, 和暦, 変換, 干支, 今日は何の日"
         />
+        <meta name="google-site-verification" content="M_9xCpXlDzN6_KIGiDz5kIjEwYxVcO452FtZMeLY0Aw" />
       </Helmet>
       <SectionLayout bgColor={L_GRAY}>
         <Header />

513f1f24c2ba64c5a067db27ab6f04ffd2c63ea6
katsuya yamaguchi <y.katsuya1116@gmail.com>
2024-07-22 09:52:20 +0900

    feat: uiコンポーネントも関数に変更

--- src/ui/Circle.tsx
+++ src/ui/Circle.tsx
@@ -1,25 +1,26 @@
-import React, { Component, ReactNode } from 'react';
+import React, { ReactNode } from 'react';
 import { Color } from '../types/color';
 
 interface CircleProps {
   bgColor: Color;
-  children: ReactNode;
   className?: string;
+  children: ReactNode;
 }
 
-class Circle extends Component<CircleProps> {
-  render() {
-    const { bgColor, children, className } = this.props;
-    const bgColorClass = `bg-${bgColor}`;
+function Circle({
+  bgColor,
+  className = '',
+  children,
+}: CircleProps): JSX.Element {
+  const bgColorClass = `bg-${bgColor}`;
 
-    return (
-      <div
-        className={`${bgColorClass} rounded-full w-72 h-72 md:size-8/12 md:w-md-circle md:h-md-circle m-auto flex flex-col items-center justify-center text-center gap-1 md:gap-9 text-sm md:text-3xl leading-relaxed md:leading-loose ${className}`}
-      >
-        {children}
-      </div>
-    );
-  }
+  return (
+    <div
+      className={`${bgColorClass} rounded-full w-72 h-72 md:size-8/12 md:w-md-circle md:h-md-circle m-auto flex flex-col items-center justify-center text-center gap-1 md:gap-9 text-sm md:text-3xl leading-relaxed md:leading-loose ${className}`}
+    >
+      {children}
+    </div>
+  );
 }
 
 export default Circle;
--- src/ui/Heading.tsx
+++ src/ui/Heading.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Color } from '../types/color';
 
 interface HeadingProps {
@@ -6,19 +6,16 @@ interface HeadingProps {
   text: string;
 }
 
-class Heading extends Component<HeadingProps> {
-  render() {
-    const { textColor, text } = this.props;
-    const textColorClass = `text-${textColor}`;
+function Heading({ textColor, text }: HeadingProps): JSX.Element {
+  const textColorClass = `text-${textColor}`;
 
-    return (
-      <h2
-        className={`${textColorClass} text-2xl md:text-7xl font-mono px-4 pt-4 text-right`}
-      >
-        {text}
-      </h2>
-    );
-  }
+  return (
+    <h2
+      className={`${textColorClass} text-2xl md:text-7xl font-mono px-4 pt-4 text-right`}
+    >
+      {text}
+    </h2>
+  );
 }
 
 export default Heading;
